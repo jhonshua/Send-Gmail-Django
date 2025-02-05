585eeb9c84a0453df428c293f95798e2d830bf26
Julio-PC\Julio <julio.llinas@gmail.com>
2025-02-05 08:20:47 -0400

    update: all project

--- core/apps.py
+++ core/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class SendMailConfig(AppConfig):
+class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'send_mail'
+    name = 'core'
---
+++ core/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render, redirect
+from django.template.loader import render_to_string
+from django.core.mail import EmailMessage
+from django.conf import settings
+from django.contrib import messages
+
+def index(request):
+    return render(request, 'index.html')
+
+def contact(request):
+
+    if request.method == 'POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        subject = request.POST['subject']
+        message = request.POST['message']
+
+        template = render_to_string('email-template.html', {
+            'name': name,
+            'email': email,
+            'subject': subject,
+            'message': message
+        })
+
+        emailSender = EmailMessage(
+            subject,
+            template,
+            settings.EMAIL_HOST_USER,
+            ['correo1@correo.com','correo2@correo.com']
+        )
+        emailSender.content_subtype = 'html'
+        emailSender.fail_silently = False
+        emailSender.send()
+
+        messages.success(request, 'El correo electrónico se envió correctamente')
+        return redirect('index')
